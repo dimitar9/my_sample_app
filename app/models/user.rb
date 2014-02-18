c50ac70e7f363a6aa92ed24f75e752b5ef0d97f4
dimitar9 <dimitar9909@gmail.com>
2014-02-18 01:06:33 -0500

    make a basic user mode including secure passwords

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ActiveRecord::Base
-  validates :name, presence: true
-  validates :email, presence: true
+  before_save { self.email = email.downcase }
+  validates :name, presence: true, length:{maximum: 50}
+  VALID_EMAIL_REGEX =      /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
+                  uniqueness: { case_sensitive: false }
+  has_secure_password #(Presence validations for the password and its confirmation are automatically added by has_secure_password.)
+  validates :password , length: {minimum: 6}
 end
