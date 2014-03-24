27c7dbeea1947170ea137cebc3a5466f4b3be8d4
dimitar9 <dimitar9909@gmail.com>
2014-03-23 23:36:06 -0400

    fix errors. wrong names, wrong locations

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,22 +3,22 @@ class UsersController < ApplicationController
                 only: [:index, :edit, :update, :destroy, :following, :followers]
   before_action :correct_user,   only: [:edit, :update]
   before_action :admin_user,     only: :destroy
-  
+
   def index
     @users = User.paginate(page: params[:page])
   end
-  
+
   def show
     @user = User.find(params[:id])
     @microposts = @user.microposts.paginate(page: params[:page])
   end
-  
+
   def new
-    @user=User.new
+    @user = User.new
   end
-  
+
   def create
-    @user = User.new(user_params)    # Not the final implementation!
+    @user = User.new(user_params)
     if @user.save
       sign_in @user
       flash[:success] = "Welcome to the Sample App!"
@@ -27,15 +27,10 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
-  
-  def destroy
-    sign_out
-    redirect_to root_url
-  end
-  
+
   def edit
   end
-  
+
   def update
     if @user.update_attributes(user_params)
       flash[:success] = "Profile updated"
@@ -43,13 +38,14 @@ class UsersController < ApplicationController
     else
       render 'edit'
     end
-  end 
-  
+  end
+
   def destroy
     User.find(params[:id]).destroy
-    flash[:success] = "User deleted."
+    flash[:success] = "User destroyed."
     redirect_to users_url
   end
+
   def following
     @title = "Following"
     @user = User.find(params[:id])
@@ -64,7 +60,7 @@ class UsersController < ApplicationController
     render 'show_follow'
   end
 
-  private  
+  private
 
     def user_params
       params.require(:user).permit(:name, :email, :password,
@@ -73,20 +69,12 @@ class UsersController < ApplicationController
 
     # Before filters
 
-    def signed_in_user
-      unless signed_in?
-        store_location
-        redirect_to signin_url, notice: "Please sign in."
-      end
-    end
-    
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_url) unless current_user?(@user)
     end
-    
+
     def admin_user
       redirect_to(root_url) unless current_user.admin?
     end
-    
-end
+  end
